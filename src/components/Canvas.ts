54d852e4478f5400f666f243aafa1216d31cef6a
AnNingUI <an3533581512@gmail.com>
2025-07-06 13:05:45 +0800

    feat(components): 添加 CanvasOnce 组件并更新导出列表
    - 在 Canvas.ts 中添加 CanvasOnce 函数，用于在 LitElement 中只初始化一次画布
    - 更新 mod.ts 导出列表，添加新的 CanvasOnce 导出项
    - 调整导出列表顺序，按组件名称排序

--- src/components/Canvas.ts
+++ src/components/Canvas.ts
@@ -1,4 +1,4 @@
-import { html, TemplateResult } from "lit";
+import { html, LitElement, TemplateResult } from "lit";
 import { createRef, ref, RefOrCallback } from "lit/directives/ref.js";
 import { createEventBinder, OnEvent } from "../core";
 
@@ -51,4 +51,16 @@ export function Canvas(
 			style=${props?.style ?? ""}
 		></canvas>
 	`;
+}
+
+export function CanvasOnce(
+	elementThis: LitElement,
+	initCanvas: () => TemplateResult<1>
+): TemplateResult<1> {
+	if ((elementThis as any).$__LYCO_INIT_CANVAS__$) {
+		return (elementThis as any).$__LYCO_INIT_CANVAS__$ as TemplateResult<1>;
+	} else {
+		(elementThis as any).$__LYCO_INIT_CANVAS__$ = initCanvas();
+		return (elementThis as any).$__LYCO_INIT_CANVAS__$ as TemplateResult<1>;
+	}
 }
